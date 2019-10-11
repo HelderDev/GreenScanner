eccb1859a3a128b43d5353a51ff039d720b530db
Helder <helder.amorim@unisys.com>
2019-10-11 14:09:48 -0300

    table view

--- GreenScanner/src/model/dao/UserDAO.java
+++ GreenScanner/src/model/dao/UserDAO.java
@@ -8,7 +8,10 @@ package model.dao;
 import connection.ConnectionFactory;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
@@ -40,4 +43,36 @@ public class UserDAO {
             ConnectionFactory.closeConnection(con, stmt);
         }
     }
+    public List <User> read(){
+         Connection con = ConnectionFactory.getConnection();
+        PreparedStatement stmt = null;
+        
+        ResultSet rs = null;
+        
+        List <User> users = new ArrayList<>();
+        
+        try {
+            stmt = con.prepareStatement("SELECT * FROM user");
+           rs = stmt.executeQuery();
+           
+           while (rs.next()){
+               User user = new User();
+               user.setId(rs.getInt("id"));
+               user.setName(rs.getString("name"));
+               user.setTitle(rs.getString("title"));
+               user.setPermission(rs.getInt("permission"));
+               user.setCreation(rs.getString("creation"));
+
+               users.add(user);
+           }
+        } catch (SQLException ex) {
+                        JOptionPane.showMessageDialog(null, "Erro ao acessar o banco" + ex);
+
+         }finally{
+            ConnectionFactory.closeConnection(con, stmt, rs);
+        }
+        
+        return users;
+    }
+    
 }
--- GreenScanner/src/view/FXMLDocumentController.java
+++ GreenScanner/src/view/FXMLDocumentController.java
@@ -16,11 +16,18 @@ import java.util.Date;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 import model.bean.User;
 import model.dao.UserDAO;
 
@@ -36,6 +43,20 @@ public class FXMLDocumentController implements Initializable {
     private TextField titleField;
     @FXML
     private TextField nameField;
+    @FXML
+    private TableView<User> usersTable;
+    @FXML
+    private TableColumn<User, String> id;
+    @FXML
+    private TableColumn<User, String> name;
+    @FXML
+    private TableColumn<User, String> title;
+    @FXML
+    private TableColumn<User, String> permission;
+    @FXML
+    private TableColumn<User, String> creation;
+
+    ObservableList<User> oblist = FXCollections.observableArrayList();
 
     @FXML
     private void handleButtonAction(ActionEvent event) {
@@ -73,11 +94,28 @@ public class FXMLDocumentController implements Initializable {
         u.setPermission(1);
         u.setCreation(formatter.format(date));
         dao.create(u);
+        readTable();
+    }
+
+    public void readTable() {
+        usersTable.getItems().clear();
+        UserDAO udao = new UserDAO();
+        for (User u : udao.read()) {
+            id.setCellValueFactory(new PropertyValueFactory<>("id"));
+            name.setCellValueFactory(new PropertyValueFactory<>("name"));
+            title.setCellValueFactory(new PropertyValueFactory<>("title"));
+            permission.setCellValueFactory(new PropertyValueFactory<>("permission"));
+            creation.setCellValueFactory(new PropertyValueFactory<>("creation"));
+
+            usersTable.getItems().add(u);
+
+        }
     }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        readTable();
     }
 
 }
